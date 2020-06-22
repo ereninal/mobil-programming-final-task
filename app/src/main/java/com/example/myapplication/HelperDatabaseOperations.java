161d0b00a6cefdc51670b6a138abebad80a7d1f1
ereninal10@gmail.com <ereninal10@gmail.com>
2020-06-22 12:25:21 +0300

    Some database operations have been added.The TODO list elemnts added.

--- app/src/main/java/com/example/myapplication/HelperDatabaseOperations.java
+++ app/src/main/java/com/example/myapplication/HelperDatabaseOperations.java
@@ -1,5 +1,62 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
 public class HelperDatabaseOperations {
-    //TODO: Countine be..
+    private static SQLiteDatabase db;
+    private static HelperDatabase helperDatabase;
+    private static ModelUsers users;
+    private static ContentValues values = new ContentValues();
+    public HelperDatabaseOperations(Context c){
+        helperDatabase = new HelperDatabase(c);
+    }
+    private static void DatabaseOpen(){
+        db = helperDatabase.getWritableDatabase();
+    }
+    private static void DatabaseClose(){
+        helperDatabase.close();
+    }
+    public void FirstUserInsert(){//bir sefer kullanıcak
+        DatabaseOpen();
+        users = new ModelUsers();
+        users.setUsername("ereninal");
+        users.setFullName("Eren İNAL");
+        users.setEmail("ereninal10@gmail.com");
+        users.setPassword("123456");
+        users.setSession("False");
+        users.setUsers(users);
+        values.put("Fullname",users.getFullName());
+        values.put("Username", users.getUsername());
+        values.put("Email", users.getEmail());
+        values.put("Password", users.getPassword());
+        values.put("Session", users.getSession());
+        db.insert("users",null,values);
+        DatabaseClose();
+
+    }
+    public void InsertUser(ModelUsers modelUsers){
+        DatabaseOpen();
+        users.setUsers(modelUsers);
+        values.put("Fullname",modelUsers.getFullName());
+        values.put("Username", modelUsers.getUsername());
+        values.put("Email", modelUsers.getEmail());
+        values.put("Password", modelUsers.getPassword());
+        values.put("Session", modelUsers.getSession());
+        db.insert("users",null,values);
+        DatabaseClose();
+        //TODO: Yeni Kullanıcı oluşturma işlemi yapılacak
+    }
+    public List<ModelUsers> GetUserAll(){
+        //TODO: Tüm kullanıcılar Listelenecek
+        return null;
+    }
+    public List<ModelUsers> GetUser(String username){
+        //TODO: gelen kullanıcı adı tüm kullanıcarda aranacak varsa list dönecek yoksa null
+        return null;
+    }
+
 }
--- app/src/main/java/com/example/myapplication/ModelUsers.java
+++ app/src/main/java/com/example/myapplication/ModelUsers.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ModelUsers implements Parcelable {
@@ -13,7 +14,65 @@ public class ModelUsers implements Parcelable {
     private String Email;
     private String Password;
     private String Session;
-    public List<ModelUsers> Users;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getSession() {
+        return Session;
+    }
+
+    public void setSession(String session) {
+        Session = session;
+    }
+
+    public List<ModelUsers> getUsers() {
+        return Users;
+    }
+
+    public void setUsers(ModelUsers users) {
+        Users.clear();
+        Users.add(users);
+    }
+
+    public List<ModelUsers> Users = new ArrayList<>();
 
     public ModelUsers(){
 
